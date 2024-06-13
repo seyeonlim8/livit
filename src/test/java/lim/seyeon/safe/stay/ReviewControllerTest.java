830e44dee73fa403628f86053f2f8f47b0f30990
Seyeon Lim <seyeonlim8@gmail.com>
2024-06-13 18:30:25 +0900

    fixed error with post method

--- src/test/java/lim/seyeon/safe/stay/ReviewControllerTest.java
+++ src/test/java/lim/seyeon/safe/stay/ReviewControllerTest.java
@@ -1,4 +1,11 @@
 package lim.seyeon.safe.stay;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
 public class ReviewControllerTest {
 }
